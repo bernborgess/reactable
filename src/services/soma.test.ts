188e04c60887cbc6459326162b070973e87be355
bernborgess <bernborgess@outlook.com>
2023-02-20 11:15:18 -0300

    sum tests

--- src/services/soma.test.ts
+++ src/services/soma.test.ts
@@ -1,11 +1,31 @@
-import { soma } from "./soma"
+import { eitherMultiply, maybeMultiply, multiply, soma } from "./soma"
 
 describe('soma', () => {
   it('should add 1 to the param number', () => {
     const value = soma(1)
     expect(value).toBe(2)
   })
-})
 
+  it('should throw error on invalid param b', () => {
+    expect(() => multiply(2, 4))
+      .toThrow('Argument b is invalid!')
+  })
+
+  it('should check for the maybeMultiply', () => {
+    const value = maybeMultiply(2, 3)
+    expect(value).toEqual({ just: 6 })
+
+    const nothing = maybeMultiply(3, 4)
+    expect(nothing).toEqual({ nothing: undefined })
+  })
+
+  it('should check for the eitherMultiply', () => {
+    const rvalue = eitherMultiply(2, 3)
+    expect(rvalue).toEqual({ right: 6 })
+
+    const lvalue = eitherMultiply(3, 4)
+    expect(lvalue).toEqual({ left: 'Argument b is invalid!' })
+  })
+})
 
 
--- src/services/soma.ts
+++ src/services/soma.ts
@@ -1,4 +1,27 @@
 
-export const soma = (a: number): number => {
-  return a + 1
+export const soma = (a: number) =>
+  a + 1
+
+export const multiply = (a: number, b: number) => {
+  if (b !== 2 && b !== 3)
+    throw new Error("Argument b is invalid!")
+  return a * b
+}
+
+//? Trying to use the Maybe Monad
+type Maybe<T> = { just: T } | { nothing: undefined }
+
+export const maybeMultiply = (a: number, b: number): Maybe<number> => {
+  if (b !== 2 && b !== 3)
+    return { nothing: undefined }
+  return { just: a * b }
+}
+
+//? Implementation with the Either type
+type Either<T, U> = { left: T } | { right: U }
+
+export const eitherMultiply = (a: number, b: number): Either<string, number> => {
+  if (b !== 2 && b !== 3)
+    return { left: "Argument b is invalid!" }
+  return { right: a * b }
 }
